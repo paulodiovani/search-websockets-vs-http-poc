6a012f63171ea0a56b52fab2f621298592c6bba4
Paulo Diovani Gonçalves <paulo@diovani.com>
2023-02-15 14:09:46 -0300

    Add eslint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { findMovies } from './lib/movies.js'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const app = express()
 
